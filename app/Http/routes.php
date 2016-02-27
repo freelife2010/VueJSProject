b119bd19327f752b2dadb8f444795aed5c9cdeb6
Eugene <eugeneoshepkov@gmail.com>
2016-02-27 17:45:16 +0300

    Add routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -102,6 +102,7 @@ $api->version('v1', ['middleware' => 'api.auth'], function ($api) {
     $api->controller('conference', 'App\API\Controllers\ConferenceAPIController');
     $api->controller('queue', 'App\API\Controllers\QueueAPIController');
     $api->controller('info', 'App\API\Controllers\InfoAPIController');
+    $api->controller('ivr', 'App\API\Controllers\IVRAPIController');
 
     //File api routes
     $api->get('voicemail/list/{user_id}', 'App\API\Controllers\FileAPIController@getVoicemailList');
