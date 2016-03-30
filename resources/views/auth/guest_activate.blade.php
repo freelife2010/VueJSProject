9d979f33494544410e708cebaaebb5c5c4ee6557
Eugene <eugeneoshepkov@gmail.com>
2016-03-30 18:45:07 +0300

    Fixes inactive user page

--- resources/views/auth/guest_activate.blade.php
+++ resources/views/auth/guest_activate.blade.php
@@ -1,21 +1,20 @@
 @extends('app')
+@section('wrapper_content')
+	<div class="block-center mt-xl wd-xl">
+		<div class="panel panel-dark panel-flat">
+			<div class="panel-heading text-center">
+				<a href="/">
+					<img src="/img/logo.png" alt="Image" class="block-center img-rounded">
+				</a>
+			</div>
+			<div class="panel-body">
+				<p>An email was sent to {{ $email }} on {{ $date }}.</p>
 
-@section('content')
-<div class="container">
-	<div class="row">
-		<div class="col-md-10 col-md-offset-1">
-			<div class="panel panel-default">
-				<div class="panel-heading">{{ Lang::get('titles.home') }}</div>
+				<p>{{ Lang::get('auth.clickInEmail') }}</p>
 
-				<div class="panel-body">
-					<p>An email was sent to {{ $email }} on {{ $date }}.</p>
-
-					<p>{{ Lang::get('auth.clickInEmail') }}</p>
-					
-					<p><a href='/resendEmail'>{{ Lang::get('auth.clickHereResend') }}</a></p>
-				</div>
+				<p><a href='/resendEmail'>{{ Lang::get('auth.clickHereResend') }}</a></p>
 			</div>
 		</div>
 	</div>
-</div>
 @endsection
+
